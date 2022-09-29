f5d90ca8c3557ca21bcaebf3816f0bd0786ccd06
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2022-09-29 10:53:04 -0700

    test

--- udiBlinkController.py
+++ udiBlinkController.py
@@ -317,7 +317,7 @@ class BlinkSetup (udi_interface.Node):
 if __name__ == "__main__":
     try:
         polyglot = udi_interface.Interface([])
-        polyglot.start('0.1.17')
+        polyglot.start('0.1.18')
         BlinkSetup(polyglot, 'controller', 'controller', 'BlinkSetup')
 
         # Just sit and wait for events

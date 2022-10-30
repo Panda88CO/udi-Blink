4233e5e8b68dc4448867bc0eb3c37d1396a2e238
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2022-10-30 12:21:13 -0700

    Update BlinkSystem.py

--- BlinkSystem.py
+++ BlinkSystem.py
@@ -223,9 +223,9 @@ class blink_system(object):
         photo_string =  camera_name+dinfo.strftime("_%m_%d_%Y-%H_%M_%S")+'.jpg'
         logging.debug('snap_picture - {} - {}'.format(camera_name, photo_string ))
         self.blink.refresh()  
-        logging.debug('after refesh {}'.format(self.blink.cameras[camera_name]))           # Get new information from server
+        logging.debug('after refesh {}'.format(self.blink.cameras[camera_name].attributes))           # Get new information from server
         time.sleep(30)
-        logging.debug('after 30 sec sleep {}'.format(self.blink.cameras[camera_name]))           # Get new information from server
+        logging.debug('after 30 sec sleep {}'.format(self.blink.cameras[camera_name].atributes))           # Get new information from server
 
         self.blink.cameras[camera_name].image_to_file('./'+photo_string)
         
